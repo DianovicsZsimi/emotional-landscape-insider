9a4589c41248694e0bc7aff962aa3f847bf62f13
fejossandor <fejos.sandor02@gmail.com>
2026-02-13 23:18:21 +0100

    Plots

--- Analysis_Sanyi.R
+++ Analysis_Sanyi.R
@@ -5,6 +5,8 @@ library(ggplot2)
 library(dplyr)
 library(stringr)
 library(rstatix)
+install.packages("ggthemes")
+library(ggthemes)
 raw_data = read.csv("D:/Emotional landscape/raw_data.csv")
 
 
@@ -33,9 +35,164 @@ data_recoded = data_recoded %>%
          feeling_anger = feeling_stage_20)
 
 data_recoded = data_recoded %>% 
-  mutate(research_stage = factor(research_stage))
+  mutate(research_stage = factor(research_stage)) %>% 
+  mutate(area = factor(area)) %>% 
+  mutate(gender = factor(gender)) %>% 
+  mutate(position = factor(position)) %>% 
+  mutate(category_of_work = factor(category_of_work)) %>% 
+  mutate(research_stage = recode(research_stage,	
+                                 "Post-project stage (the project has received a decision (accepted or rejected). At this point, it is either concluded or being revised and prepared for submission to another venue)"
+                                 = "Post-project stage", 
+                                 "Preparations (IRB permit, preregistration, experiment preparations)" = 
+                                   "Preparations",
+                                 "Reality check (e.g., literature search, consultation with colleagues)" =
+                                   "Reality check"
+                                 
+  ))
+  
 levels(data_recoded$research_stage)
 
-research_stage_count = data.frame(levels(data_recoded$research_stage[2])
+research_stage_count = data_recoded %>% 
+  count(research_stage) %>% 
+  slice(-1) 
+ 
 
-                                  
+area_count = data_recoded %>% 
+  count(area) %>% 
+  slice(-1)
+
+gender_count = data_recoded %>% 
+  count(gender) %>% 
+  slice(-1)
+
+position_count = data_recoded %>% 
+  count(position) %>% 
+  slice(-1)
+
+category_of_work_count = data_recoded %>% 
+  count(category_of_work) %>% 
+  slice(-1)
+
+plot_resarch_stage_count = research_stage_count %>%
+  arrange(n) %>%
+  mutate(research_stage = reorder(research_stage, n)) %>%
+  ggplot(aes(x = research_stage, y = n)) +
+  geom_col(width = 0.7) +
+  geom_text(aes(label = n),
+            hjust = -0.1,
+            size = 4) +
+  coord_flip() +
+  theme_tufte() +
+  theme(plot.title = element_text(face = "bold", hjust = 0.5))
+  labs(x = NULL,
+       y = "Count (n)",
+       title = "Research Stage") +
+  expand_limits(y = max(research_stage_count$n) * 1.1)
+plot_resarch_stage_count
+
+plot_area_count = area_count %>%
+  arrange(n) %>%
+  mutate(area = reorder(area, n)) %>%
+  ggplot(aes(x = area, y = n)) +
+  geom_col(width = 0.7) +
+  geom_text(aes(label = n),
+            hjust = -0.1,
+            size = 4) +
+  coord_flip() +
+  theme_tufte() +
+  theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
+  labs(x = NULL,
+       y = "Count (n)",
+       title = "Area") +
+  expand_limits(y = max(area_count$n) * 1.1)
+plot_area_count
+
+plot_gender_count = gender_count %>%
+  arrange(n) %>%
+  mutate(gender = reorder(gender, n)) %>%
+  ggplot(aes(x = gender, y = n)) +
+  geom_col(width = 0.7) +
+  geom_text(aes(label = n),
+            hjust = -0.1,
+            size = 4) +
+  coord_flip() +
+  theme_tufte() +
+  theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
+  labs(x = NULL,
+       y = "Count (n)",
+       title = "Gender") +
+  expand_limits(y = max(gender_count$n) * 1.1)
+
+plot_gender_count
+
+plot_position_count = position_count %>%
+  arrange(n) %>%
+  mutate(position = reorder(position, n)) %>%
+  ggplot(aes(x = position, y = n)) +
+  geom_col(width = 0.7) +
+  geom_text(aes(label = n),
+            hjust = -0.1,
+            size = 4) +
+  coord_flip() +
+  theme_tufte() +
+  theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
+  labs(x = NULL,
+       y = "Count (n)",
+       title = "Position") +
+  expand_limits(y = max(position_count$n) * 1.1)
+
+plot_position_count
+
+plot_category_of_work_count = category_of_work_count %>%
+  arrange(n) %>%
+  mutate(category_of_work = reorder(category_of_work, n)) %>%
+  ggplot(aes(x = category_of_work, y = n)) +
+  geom_col(width = 0.7) +
+  geom_text(aes(label = n),
+            hjust = -0.1,
+            size = 4) +
+  coord_flip() +
+  theme_tufte() +
+  theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
+  labs(x = NULL,
+       y = "Count (n)",
+       title = "Category of Work") +
+  expand_limits(y = max(category_of_work_count$n) * 1.1)
+
+plot_category_of_work_count
+
+
+
+#overall emotions visualization by research stage
+
+research_stage_emotions = data_recoded %>% 
+  select(research_stage, starts_with("feeling_") )
+
+research_stage_emotions = research_stage_emotions %>% 
+  select(-22)
+research_stage_emotions = research_stage_emotions %>%   
+  mutate(across(starts_with("feeling_"),
+                ~ as.numeric(as.character(.x)))) %>% 
+  mutate(across(12:21, ~ .x * -1)) %>% 
+  drop_na() %>% 
+  group_by(research_stage) %>%
+  summarise(across(starts_with("feeling_"),
+                   ~ mean(.x, na.rm = TRUE),
+                   .names = "mean_{.col}"))
+
+research_stage_emotions_mean = research_stage_emotions %>% 
+  mutate(mean_emotion = rowMeans(across(c(starts_with("mean_"))))) %>% 
+  select(research_stage, mean_emotion) %>% 
+  arrange(mean_emotion)
+ 
+research_stage_emotions_mean %>% 
+  arrange(mean_emotion) %>% 
+  ggplot()+
+  aes(y = reorder(research_stage, mean_emotion), x = mean_emotion, fill = mean_emotion)+ 
+  geom_col(width = 0.3)+
+  scale_fill_gradient(low = "orange", high = "red")+
+  theme_tufte()+
+  labs(title = "Valence per stage", x = "", y = "")+
+  theme(axis.text.y = element_text(size = 11, angle = 45, vjust = -3))+
+  theme(plot.title = element_text(angle = 0, hjust = 0.5, face = "bold"))
+  
